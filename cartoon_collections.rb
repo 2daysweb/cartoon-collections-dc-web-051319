54f613e7cbbf2b319ae550a8f4c1177a600dea8a
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 17:18:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |planeteer_call|
+    planeteer_call
 end
 
 def long_planeteer_calls# code an argument here
