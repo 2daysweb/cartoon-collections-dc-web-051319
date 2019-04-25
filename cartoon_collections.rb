b1c2cbe66a3717023bbe509f9b01c7c963f0db43
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 17:35:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheesea(array)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |ingredient|
   if (cheese_types.include? : ingredient)
